1f9f250b542e0a7d329992d5908aeacf3bc7fa67
Alejandro Napoles <alejandrodevnapoles@pm.me>
2026-07-16 13:02:27 +0100

    Split server tests

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -11,24 +11,8 @@ import (
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/status"
 )
-
-func TestServer(t *testing.T) {
-	for scenario, fn := range map[string]func(
-		t *testing.T,
-		client api.LogClient,
-		config *Config){
-		"produce/consume a message to/from the log succeeds": testProduceConsume,
-		//"produce/consume stream succeeds":                    testProduceConsumeStream,
-		//"consume past log boundary fails":                    testConsumePastBoundary,
-	} {
-		t.Run(scenario, func(t *testing.T) {
-			client, config, teardown := setupTest(t, nil)
-			defer teardown()
-			fn(t, client, config)
-		})
-	}
-}
 
 func setupTest(t *testing.T, fn func(*Config)) (
 	client api.LogClient,
@@ -76,7 +60,10 @@ func setupTest(t *testing.T, fn func(*Config)) (
 	}
 }
 
-func testProduceConsume(t *testing.T, client api.LogClient, config *Config) {
+func TestProduceConsume(t *testing.T) {
+	client, _, teardown := setupTest(t, nil)
+	t.Cleanup(teardown)
+
 	ctx := context.Background()
 
 	want := &api.Record{
@@ -94,4 +81,78 @@ func testProduceConsume(t *testing.T, client api.LogClient, config *Config) {
 	require.NoError(t, err)
 	require.Equal(t, want.Value, consume.Record.Value)
 	require.Equal(t, want.Offset, consume.Record.Offset)
+}
+
+func TestConsumePastBoundary(t *testing.T) {
+	client, _, teardown := setupTest(t, nil)
+	t.Cleanup(teardown)
+
+	ctx := context.Background()
+
+	produce, err := client.Produce(ctx, &api.ProduceRequest{
+		Record: &api.Record{
+			Value: []byte("hello world"),
+		},
+	})
+
+	require.NoError(t, err)
+
+	consume, err := client.Consume(ctx, &api.ConsumeRequest{
+		Offset: produce.Offset + 1,
+	})
+
+	if consume != nil {
+		t.Fatal("consume not nil")
+	}
+
+	got := status.Code(err)
+	want := status.Code(api.ErrOffsetOutOfRange{}.GRPCStatus().Err())
+
+	if got != want {
+		t.Fatalf("got error %v, want %v", got, want)
+	}
+}
+
+func TestProduceConsumeStream(t *testing.T) {
+	client, _, teardown := setupTest(t, nil)
+	t.Cleanup(teardown)
+
+	ctx := context.Background()
+
+	records := []*api.Record{{
+		Value:  []byte("first message"),
+		Offset: 0,
+	}, {
+		Value:  []byte("second message"),
+		Offset: 1,
+	}}
+	{
+		stream, err := client.ProduceStream(ctx)
+		require.NoError(t, err)
+
+		for offset, record := range records {
+			err = stream.Send(&api.ProduceRequest{
+				Record: record,
+			})
+
+			require.NoError(t, err)
+
+			res, err := stream.Recv()
+			require.NoError(t, err)
+
+			if res.Offset != uint64(offset) {
+				t.Fatalf("got offset %d, want %d", res.Offset, offset)
+			}
+		}
+	}
+	{
+		stream, err := client.ConsumeStream(ctx, &api.ConsumeRequest{Offset: 0})
+		require.NoError(t, err)
+
+		for i, record := range records {
+			res, err := stream.Recv()
+			require.NoError(t, err)
+			require.Equal(t, res.Record, &api.Record{Value: record.Value, Offset: uint64(i)})
+		}
+	}
 }
