e9b743417621b0993385d2a6cc769125fdd25f2c
Alejandro Napoles <alejandrodevnapoles@pm.me>
2026-07-16 10:29:11 +0100

    Test produce/consume a message to/from the log

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -1,12 +1,16 @@
 package server
 
 import (
+	"context"
 	"net"
+	"os"
 	"testing"
 
 	api "github.com/lexnapoles/proglog/api/v1"
+	"github.com/lexnapoles/proglog/internal/log"
 	"github.com/stretchr/testify/require"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func TestServer(t *testing.T) {
@@ -15,8 +19,8 @@ func TestServer(t *testing.T) {
 		client api.LogClient,
 		config *Config){
 		"produce/consume a message to/from the log succeeds": testProduceConsume,
-		"produce/consume stream succeeds":                    testProduceConsumeStream,
-		"consume past log boundary fails":                    testConsumePastBoundary,
+		//"produce/consume stream succeeds":                    testProduceConsumeStream,
+		//"consume past log boundary fails":                    testConsumePastBoundary,
 	} {
 		t.Run(scenario, func(t *testing.T) {
 			client, config, teardown := setupTest(t, nil)
@@ -36,6 +40,58 @@ func setupTest(t *testing.T, fn func(*Config)) (
 	l, err := net.Listen("tcp", ":0")
 	require.NoError(t, err)
 
-	clientOptions := []grpc.DialOption{grpc.WithInsecure()}
+	clientOptions := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
 
+	cc, err := grpc.NewClient(l.Addr().String(), clientOptions...)
+	require.NoError(t, err)
+
+	dir, err := os.MkdirTemp("", "server-test")
+	require.NoError(t, err)
+
+	clog, err := log.NewLog(dir, log.Config{})
+	require.NoError(t, err)
+
+	cfg = &Config{
+		CommitLog: clog,
+	}
+
+	if fn != nil {
+		fn(cfg)
+	}
+
+	server, err := NewGRPCServer(cfg)
+	require.NoError(t, err)
+
+	go func() {
+		server.Serve(l)
+	}()
+
+	client = api.NewLogClient(cc)
+
+	return client, cfg, func() {
+		server.Stop()
+		cc.Close()
+		l.Close()
+		clog.Remove()
+	}
+}
+
+func testProduceConsume(t *testing.T, client api.LogClient, config *Config) {
+	ctx := context.Background()
+
+	want := &api.Record{
+		Value: []byte("hello world"),
+	}
+
+	produce, err := client.Produce(ctx, &api.ProduceRequest{
+		Record: want,
+	})
+
+	require.NoError(t, err)
+
+	consume, err := client.Consume(ctx, &api.ConsumeRequest{Offset: produce.Offset})
+
+	require.NoError(t, err)
+	require.Equal(t, want.Value, consume.Record.Value)
+	require.Equal(t, want.Offset, consume.Record.Offset)
 }
